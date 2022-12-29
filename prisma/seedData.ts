83dec6e3a730adc16632c9aa5b1436de95dbaa97
Obi Baratt <obibaratt@gmail.com>
2022-12-28 23:09:01 -0800

    Prisma update: Schema, add importable, seed file and data.

---
+++ prisma/seedData.ts
@@ -0,0 +1,32 @@
+export const seedData = [
+  {
+    userId: "0e3bc956-5f09-4a5f-a895-ba0798471c58",
+    title: "Task 1",
+    text: "Text for task 1",
+    category: "TODO",
+  },
+  {
+    userId: "0e3bc956-5f09-4a5f-a895-ba0798471c58",
+    title: "Task 2",
+    text: "Text for task 2",
+    category: "TODO",
+  },
+  {
+    userId: "0e3bc956-5f09-4a5f-a895-ba0798471c58",
+    title: "Task 3",
+    text: "Text for task 3",
+    category: "TODO",
+  },
+  {
+    userId: "0e3bc956-5f09-4a5f-a895-ba0798471c58",
+    title: "Task 4",
+    text: "Text for task 4",
+    category: "TODO",
+  },
+  {
+    userId: "0e3bc956-5f09-4a5f-a895-ba0798471c58",
+    title: "Task 5",
+    text: "Text for task 5",
+    category: "TODO",
+  },
+];
