5f81eb780cb269d3aef8fd9eec4b76b25f5d5c60
Obi Baratt <obibaratt@gmail.com>
2022-12-29 20:41:16 -0800

    Comment out seed code

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,27 @@
-import { PrismaClient } from "@prisma/client";
-import { seedData } from "./seedData";
+// import { PrismaClient } from "@prisma/client";
+// import { seedData } from "./seedData";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-    await prisma.user.create({
-        data: {
-            id: "0e3bc956-5f09-4a5f-a895-ba0798471c58",
-            email: "admin@prototype.com",
-            role: "ADMIN",
-        },
-    });
+// async function main() {
+//     await prisma.user.create({
+//         data: {
+//             id: "0e3bc956-5f09-4a5f-a895-ba0798471c58",
+//             email: "admin@prototype.com",
+//             role: "ADMIN",
+//         },
+//     });
 
-    await prisma.task.createMany({
-        data: seedData,
-    })
-};
+//     await prisma.task.createMany({
+//         data: seedData,
+//     })
+// };
 
-main()
-.catch((e) => {
-    console.error(e);
-    process.exit(1);
-})
-.finally(async () => {
-    await prisma.$disconnect();
-});
+// main()
+// .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+// })
+// .finally(async () => {
+//     await prisma.$disconnect();
+// });
