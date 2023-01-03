2fe66414f544e2853d34a3ca8b349144665334ca
Obi Baratt <obibaratt@gmail.com>
2023-01-02 22:38:58 -0800

    Fixed negative overflow to 1hr bug, add work/rest cycling.

--- context/pomodoroContext.tsx
+++ context/pomodoroContext.tsx
@@ -7,17 +7,17 @@ import React, {
 } from "react";
 
 export interface PomodoroContextTypes {
-  isRunning: boolean;
   timer: number;
   start: () => void;
   stop: () => void;
+  isResting: boolean;
 }
 
 export const PomodoroContext = createContext<PomodoroContextTypes>({
-  isRunning: false,
   timer: 0,
   start: () => {},
   stop: () => {},
+  isResting: false,
 });
 
 // Times in seconds, so default work is 25min, break 5
@@ -28,16 +28,23 @@ const DEFAULT_TIMES = {
 
 export const PomodoroProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [isRunning, setIsRunning] = useState<boolean>(false);
-  const [timer, setTimer] = useState<number>(DEFAULT_TIMES.work);
+  const [isResting, setIsResting] = useState<boolean>(false);
+  const [timer, setTimer] = useState<number>(-1);
 
   useEffect(() => {
-    if (isRunning) {
+    if (isRunning && timer > 0) {
       const intervalId = setInterval(() => {
         setTimer(timer - 1);
       }, 1000);
       return () => clearInterval(intervalId);
+    } else if (timer === 0 && !isResting) {
+      setIsResting(true);
+      rest();
+    } else if (isResting) {
+      setIsResting(false);
+      setTimer(DEFAULT_TIMES.work);
     }
-  }, [isRunning, timer]);
+  }, [isRunning, timer, isResting]);
 
   const start = () => {
     console.log("starting pomodoro");
@@ -48,11 +55,18 @@ export const PomodoroProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const stop = () => {
     console.log("stopping pomodoro");
     setIsRunning(false);
-    setTimer(0);
+    setIsResting(false);
+    setTimer(-1);
+  };
+
+  const rest = () => {
+    console.log("time for a break!");
+    setIsResting(true);
+    setTimer(DEFAULT_TIMES.rest);
   };
 
   return (
-    <PomodoroContext.Provider value={{ isRunning, start, stop, timer }}>
+    <PomodoroContext.Provider value={{ start, stop, timer, isResting }}>
       {children}
     </PomodoroContext.Provider>
   );
--- pages/pomodoro.tsx
+++ pages/pomodoro.tsx
@@ -1,23 +1,38 @@
 import type { NextPage } from "next";
 import { useContext, useEffect } from "react";
-import {
-  PomodoroContext,
-} from "../context/pomodoroContext";
+import { PomodoroContext } from "../context/pomodoroContext";
 
 const Pomodoro: NextPage = () => {
-  const { start, stop, timer } = useContext(PomodoroContext);
+  const { start, stop, timer, isResting } = useContext(PomodoroContext);
 
   const handleDate = (seconds: number) => {
-    let date = new Date(0)
-    date.setSeconds(seconds)
-    let timeString = date.toISOString().substring(11, 19);
-    return timeString.slice(3);
-  }
+    let date = new Date(0);
+    date.setSeconds(seconds);
+    let timeString = date.toISOString().substring(14, 19);
+    return timeString;
+  };
+
+  const handleDisplay = () => {
+    if (timer > 0) {
+      return (
+        <>
+          {isResting ? (
+            <h3>Resting for: {handleDate(timer)}</h3>
+          ) : (
+            <h3>Working for: {handleDate(timer)}</h3>
+          )}
+        </>
+      );
+    } else {
+      return <h3>Ready? Click the start button!</h3>;
+    }
+  };
 
   return (
     <>
-      <div>POMODORO GOES HERE</div>
-      <h1>{handleDate(timer)}</h1>
+      <h1>Pomodoro Timer</h1>
+      {handleDisplay()}
+
       <button onClick={start}>Start Timer</button>
       <button onClick={stop}>Stop Timer</button>
     </>
