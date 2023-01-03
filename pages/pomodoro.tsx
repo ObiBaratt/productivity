8cb6679936dcc15d851f667f05ae147edf52bd64
Obi Baratt <obibaratt@gmail.com>
2023-01-02 20:46:47 -0800

    Update pomodoro timer to display mm:ss

--- pages/pomodoro.tsx
+++ pages/pomodoro.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import {
   PomodoroContext,
 } from "../context/pomodoroContext";
@@ -7,10 +7,17 @@ import {
 const Pomodoro: NextPage = () => {
   const { start, stop, timer } = useContext(PomodoroContext);
 
+  const handleDate = (seconds: number) => {
+    let date = new Date(0)
+    date.setSeconds(seconds)
+    let timeString = date.toISOString().substring(11, 19);
+    return timeString.slice(3);
+  }
+
   return (
     <>
       <div>POMODORO GOES HERE</div>
-      <h1>{timer}</h1>
+      <h1>{handleDate(timer)}</h1>
       <button onClick={start}>Start Timer</button>
       <button onClick={stop}>Stop Timer</button>
     </>
