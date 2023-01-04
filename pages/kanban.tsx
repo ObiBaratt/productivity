e6c72d38fe336f1f1117c83a75ee7bba0efa8fe5
Obi Baratt <obibaratt@gmail.com>
2023-01-03 22:34:27 -0800

    Add Link to alternate Kanban project.

--- pages/kanban.tsx
+++ pages/kanban.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
-
+import { MdArrowForward } from 'react-icons/md'
 
 const Kanban: NextPage  = () => {
     return (
         <>
             <div>KANBAN GOES HERE</div>
+            <p>In the meantime, check out the old version of my
+            <a
+            href="https://kanban-ob.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          > Kanban board! <MdArrowForward /></a></p>
         </>
     )
 }
