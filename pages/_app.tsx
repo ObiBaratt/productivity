d5d135b46a4c09cb066fe46dd7171bc1ab18d911
Obi Baratt <obibaratt@gmail.com>
2023-01-19 09:38:51 -0800

    Upgrade Next features.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 import { Analytics } from "@vercel/analytics/react";
 import Layout from "../apps/Global/Layout";
 import { TaskProvider } from "../context/taskContext";
@@ -8,13 +9,15 @@ import { PomodoroProvider } from "../context/pomodoroContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <TaskProvider>
-        <PomodoroProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </PomodoroProvider>
-      </TaskProvider>
+      <MantineProvider>
+        <TaskProvider>
+          <PomodoroProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </PomodoroProvider>
+        </TaskProvider>
+      </MantineProvider>
       <Analytics />
     </>
   );
